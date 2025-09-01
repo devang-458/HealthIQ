6b7fb748a1947664428257856c18cf0c0964d35f
Devang <deekayedm@gmail.com>
2025-09-01 16:32:38 +0530

    added minor changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  output: 'standalone',
+  images: {
+    domains: ['localhost', process.env.NEXT_PUBLIC_API_URL],
+  },
+  experimental: {
+    serverActions: true,
   },
 };
 
